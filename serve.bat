c2ecbe3cd835f51e7eff02be2f62867e9f7125c5
Tom Scheler <dd-tom@web.de>
2020-04-26 19:09:07 +0200

    initial commit

---
+++ serve.bat
@@ -0,0 +1,2 @@
+docker run --rm -p 80:80 -v %CD%:/usr/share/nginx/html nginx:latest
+pause
---
+++ service-worker.js
@@ -0,0 +1,38 @@
+var CACHE_NAME = 'my-site-cache-v4';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.js',
+  '/icon.png'
+];
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        var fetchPromise = fetch(event.request).then(function(networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        })
+        return response || fetchPromise;
+      })
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log("activate");
+  event.waitUntil(clients.claim());
+});
